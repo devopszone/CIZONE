20edb2a9d72234db01dc52eccafd4f2335d40dd5
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2017-01-03 16:34:45 +0530

    Added Sample Java Project

---
+++ MavenZone/HelloWorld/src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+package main.java;
+
+/**
+ * Created by khaja on 1/3/2017.
+ */
+public class HelloWorld {
+
+    public static void main(String args[])
+    {
+        System.out.println("hello World");
+    }
+
+}
